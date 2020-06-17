edb893b22253a0347f778be98f0a48dff644e7de
cox512 <64932698+cox512@users.noreply.github.com>
2020-06-17 18:39:14 -0500

    Page 2. Answers in input form load into the table successfully

--- portfolio/gimme10/app.js
+++ portfolio/gimme10/app.js
@@ -1,4 +1,5 @@
-const roundOneQuestions = ["Briefly describe a character who you associate with this word", "Name an occupation that you might associate with this word", "If this word were a character in a comicbook, what would be its defining flaw"]
+const roundOneQuestion = ["Briefly describe a character who you associate with this word", "Name an occupation that you might associate with this word", "If this word were a character in a comicbook, what would be its defining flaw"]
+
 
 $(() => {
     //WORDSAPI SET-UP. CONTACT FOR WORD.
@@ -13,24 +14,63 @@ $(() => {
         }
     }
 
-
-    //SET A VARIABLE FOR THE GETWORD COMMAND
-    //let getWord = $.ajax(randomWord);
-
     //DISPLAY THE RANDOMLY GENERATED WORD ALONG WITH ITS DEFINITION (stretch goal -- display ALL the definitions that are also nouns.)
-    let displayWord = () => {
+    const displayWord = () => {
         $.ajax(randomWord).then((response) => {
             console.log(response);
-            $("#display-word").addClass('new-word').html(`
+            $("#display-word").html(`
             <h2> ${response.word} </h2>
-            <h4> <i>${response.results[0].definition}<i> </h4>
+            <h4> <i>${response.results[0].definition}</i> </h4>
             `)
         })
     }
 
+    //Pick a random question from the roundOneQuestion array and display it.
+    const pickFirstQuestion = () => {
+        let randomNumber = Math.floor((Math.random()) * roundOneQuestion.length);
+        $("#question-field").html(`
+            <h4 class="question"> ${roundOneQuestion[randomNumber]} </h4>
+        `);
+        //console.log(roundOneQuestion[randomNumber]);
+    }
 
     const getWord = $("#get-word").on('click', () => {
         displayWord();
+        pickFirstQuestion();
     })
 
+    let repNum = 1;
+
+    const addAnswer = () => {
+        if (repNum <= 10) {
+        event.preventDefault();
+        console.log("submit works");
+        //create a new table row
+        const answerRow = $('<tr>');
+        //create the rep # data cell. Add the value of i
+        const repNumber = $('<td>').addClass('rep').text(repNum);
+        const inputValue = $('#answer').val();
+        //create the answer data cell. Add the input Value.
+        const answer = $('<td>').addClass('answer').text(inputValue)
+        $(answerRow).append(repNumber, answer);
+        $('tbody').append(answerRow);
+        $('#answer').val('');
+        repNum++;
+        }
+        if (repNum>10) {
+            //Have a modal come up
+            alert("You've completed your first set")
+        }
+        
+    }
+
+    
+    $('#submit').on('click', addAnswer);
+
+    // const answerListener = $('#submit').on('click', () => {
+    //         console.log("submit works");
+    //         addAnswer();
+    //     })
+
+    
 });

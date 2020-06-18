04ca6bb1beb10f3ed29cbb686589e3b8a392174d
cox512 <64932698+cox512@users.noreply.github.com>
2020-06-18 17:21:00 -0500

    set up local storage for recording the first set answers

--- portfolio/gimme10/app.js
+++ portfolio/gimme10/app.js
@@ -39,38 +39,84 @@ $(() => {
         pickFirstQuestion();
     })
 
-    let repNum = 1;
-
+    let repNum = 0;
     const addAnswer = () => {
-        if (repNum <= 10) {
-        event.preventDefault();
-        console.log("submit works");
-        //create a new table row
-        const answerRow = $('<tr>');
-        //create the rep # data cell. Add the value of i
-        const repNumber = $('<td>').addClass('rep').text(repNum);
-        const inputValue = $('#answer').val();
-        //create the answer data cell. Add the input Value.
-        const answer = $('<td>').addClass('answer').text(inputValue)
-        $(answerRow).append(repNumber, answer);
-        $('tbody').append(answerRow);
-        $('#answer').val('');
-        repNum++;
+        if (repNum < 1) {
+            //console.log("submit works");
+            //create variables for: new table row, value of input field, and the rep number.
+            const answerRow = $('<tr>').addClass('table-row');
+            const inputValue = $('#answer-field').val();
+            const repNumber = $('<td>').addClass('rep').text(repNum);
+            //create the answer data cell. Add the input Value.
+            const answer = $('<td>').addClass('answer').text(inputValue);
+            //Append the data cells to the row and the row to the table body.
+            $(answerRow).append(repNumber, answer);
+            $('tbody').append(answerRow);
+            storeRep();
+            clearText();
+            repNum++;
+        } else {
+            //console.log("11th time");
+            //Repeat the actions above, but for the 10th answer. This avoids the "plus one" problem
+            const answerRow = $('<tr>').addClass('table-row');
+            const inputValue = $('#answer-field').val();
+            const repNumber = $('<td>').addClass('rep').text(repNum);
+            const answer = $('<td>').addClass('answer').text(inputValue);
+            $(answerRow).append(repNumber, answer);
+            $('tbody').append(answerRow);
+            storeRep();
+            repNum++;
+            //Remove the answer section and add a the "submit set" button via buildSetSubmit.
+            $('#answer-section').remove();
+            buildSetSubmit();
         }
-        if (repNum>10) {
-            //Have a modal come up
-            alert("You've completed your first set")
-        }
-        
     }
 
+    const clearText= () => {
+        $('#answer-field').val('');
+    }
+
+    const buildSetSubmit = () => {
+        const setSubmitBtn = $('<button>');
+        setSubmitBtn.attr('type', 'button');
+        setSubmitBtn.attr('id', 'submit-set');
+        setSubmitBtn.text('Submit Set');
+        $('#set-list').append(setSubmitBtn);
+        //$('#submit-set').on('click', setOneStorage);
+    }
     
+    const storeRep = () => {
+        //console.log("button works");
+        localStorage.setItem('set1-answers', $('.answer')[repNum].innerHTML);
+        //console.log($('.answer')[0].innerHTML);
+        storageCheck();
+    }
+
+    let setOne = [];
+    const storageCheck = () => {
+        if(localStorage.getItem('set1-answers')) {
+            console.log('set1-answers exists')
+            setOne.push(localStorage.getItem('set1-answers'));
+        }
+        console.log(setOne);
+
+    }
+
     $('#submit').on('click', addAnswer);
 
-    // const answerListener = $('#submit').on('click', () => {
-    //         console.log("submit works");
-    //         addAnswer();
-    //     })
+    // //Experimenting with localStorage
+    // const arrSetOne = [];
+    // const addSet = () => {
+    //     //getData
+    //     //arrSetOne.push($('.rep').val());
+    //     arrSetOne.push($('.answer').val());
+    // }
 
+    // localStorage.setItem("", JSON.stringify(arrSetOne))
+
+
+
+     //Have a modal come up on Finish Set click
+     //Give modal two buttons one to save and continue. One to save and quit.
     
 });
